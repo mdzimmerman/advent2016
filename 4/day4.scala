368bb665d4fbd2bc711cb9be68e6467f717e2f30
Matthew Zimmerman <matt.zimmerman@ccri.com>
2016-12-05 13:32:52 -0500

    Second part of day 4.

--- 4/day4.scala
+++ 4/day4.scala
@@ -44,12 +44,31 @@ case class RoomName(encrypted: String, sectorId: Int, checksum: String) {
     checksum == realChecksum
   }
 
+  def rotChar(c: Char, rotate: Int): Option[Char] = {
+    val rot = rotate % 26
+    if (c == '-') {
+      Some(' ')
+    } else if (c >= 'a' && c <= 'z') {
+      if ((c + rot) > 'z') {
+        Some((c + rot - 26).toChar)
+      } else {
+        Some((c + rot).toChar)
+      }
+    } else {
+      None
+    }
+  }
+
+  def decryptedName(): String = {
+    encrypted.toList.flatMap(rotChar(_, sectorId)).mkString("")
+  }
+
   override def toString(): String = {
     s"RoomName(encrypted=$encrypted, sectorId=$sectorId, checksum=$checksum)"
   }
 }
 
-object RoomNameString {
+object RoomName {
   val RoomNameRegex = "(.+)-(\\d+)\\[(.+)\\]".r
 
   object AsInt {
@@ -61,17 +80,17 @@ object RoomNameString {
       }
   }
 
-  def unapply(s: String): Option[RoomName] = s match {
+  def parseString(s: String): Option[RoomName] = s match {
     case RoomNameRegex(name, AsInt(sectorId), checksum) => Some(RoomName(name, sectorId, checksum))
     case _ => None
   }
 }
 
 def sumRooms(input: List[String]): Int = {
-  input.flatMap(_ match {
-    case RoomNameString(p) => Some(p)
-    case _ => None
-  }).filter(_.validateChecksum()).map(_.sectorId).sum
+  input.flatMap(RoomName.parseString)
+      .filter(_.validateChecksum)
+        .map(_.sectorId)
+          .sum
 }
 
 val test = List(
@@ -80,8 +99,16 @@ val test = List(
   "not-a-real-room-404[oarel]",
   "totally-real-room-200[decoy]"
 )
-println(s"test  = ${sumRooms(test)}")
-
 val input = Source.fromFile("input.txt").getLines.toList
+
+println("### Part 1 ###")
+println(s"test  = ${sumRooms(test)}")
 println(s"input = ${sumRooms(input)}")
+
+println("### Part 2 ###")
+input.flatMap(RoomName.parseString).filter(_.validateChecksum).map(r => {
+  s"${r.decryptedName} => ${r.sectorId}"
+}).filter(_.contains("north")).foreach(println)
+
+
 
