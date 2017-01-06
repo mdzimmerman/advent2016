e40dc1c3e666780459b8d02bc86968d2d1cc9b20
Matthew Zimmerman <matt.zimmerman@ccri.com>
2017-01-06 13:46:14 -0500

    My implementation was too slow to do Part #2; adapted a new approach from Reddit.

--- 19/day19.scala
+++ 19/day19.scala
@@ -1,67 +1,86 @@
-class Exchange(n: Int, mode: String = "next", debug: Boolean = false) {
-  val presents = Array.fill[Int](n)(1)
+// requires ``scala -J-Xmx1g`` (or more)
 
-  def countWithPresents(): Int = presents.count(_ > 0)
+import scala.annotation.tailrec
 
-  def next(i: Int): Int = if (i == n-1) 0 else i+1
+case class Elf(id: Integer) {
+  var prev: Option[Elf] = None
+  var next: Option[Elf] = None
 
-  def nextWithPresents(i0: Int): Int = {
-    var i = next(i0)
-    while (presents(i) == 0)
-      i = next(i)
-    i
+  def getPrev: Elf = prev.get
+
+  def getNext: Elf = next.get
+
+  def delete(): Unit = {
+    prev.foreach(p => p.next = next)
+    next.foreach(n => n.prev = prev)
   }
 
-  def nextTake(i: Int): Int = {
-    val before = (0 until i).filter(presents(_) > 0)
-    val after  = (i+1 until n).filter(presents(_) > 0)
-    val others = after
-    val take = others.length/2 - 1 + others.length%2
-    others(take)
-  }
+  // generate a list of the circle, starting at the current Elf
+  // (by starting at the end and working forward)
+  def getCircle(): List[Elf] = getCircle(List(getPrev))
 
-  def offset(i: Int): Int = i+1
-
-  def run(): Int = {
-    var t = 0
-    var i = 0
-    var done = false
-    while (true) {
-      val iNext = nextWithPresents(i)
-      if (i == iNext) return offset(i)
-      if (presents(i) > 0) {
-        val iTake = if (mode == "next") iNext else nextTake(i)
-        if (debug) println(s"${offset(i)}: takes ${presents(iTake)} present(s) from ${offset(iTake)}")
-        presents(i) += presents(iTake)
-        presents(iTake) = 0
-      }
-      else {
-        if (debug) println(s"${i+1}: skipped")
-      }
-      if (t % 1000 == 0)
-        println(t)
-      t += 1
-      i = iNext
-    }
-    offset(i) // change to 1-offset (should never get here)
+  @tailrec
+  private def getCircle(circle: List[Elf]): List[Elf] = {
+    if (circle.head == this)
+      circle
+    else
+      getCircle(circle.head.getPrev :: circle)
   }
 }
 
-println("--- test next ---")
-val test = new Exchange(5, debug=true)
-println(test.run())
+object Elf {
+  def buildCircle(n: Int): Vector[Elf] = {
+    val elves = (1 to n).map(Elf(_)).toVector
+    for (i <- 0 until n - 1) {
+      elves(i).next = Some(elves(i + 1))
+      elves(i + 1).prev = Some(elves(i))
+    }
+    elves.head.prev = Some(elves.last)
+    elves.last.next = Some(elves.head)
+    elves
+  }
+}
+
+def solve(n: Int): Elf = {
+  val elves = Elf.buildCircle(n)
+
+  var start = elves.head
+  var next  = start.getNext
+  for (i <- 0 until n-1) {
+    if (i % 1000 == 0) println(s"step $i: $start <- $next")
+    next.delete()
+    start = start.getNext
+    next  = start.getNext
+  }
+  start
+}
+
+def solve2(n: Int): Elf = {
+  val elves = Elf.buildCircle(n)
+
+  var start = elves.head
+  var mid   = elves(n/2)
+  for (i <- 0 until n-1) {
+    if (i % 1000 == 0) println(s"step $i: $start <- $mid")
+    mid.delete()
+    mid = mid.getNext
+    if ((n-i)%2 == 1) mid = mid.getNext
+    start = start.getNext
+  }
+  start
+}
+
+println("--- test part #1 ---")
+println(solve(5))
 println()
 
-println("--- test across ---")
-//val test2 = new Exchange(5, mode="across", debug=true)
-//println(test2.run())
+println("--- test part #2 ---")
+println(solve2(5))
 println()
 
-println("--- input next ---")
-val input = new Exchange(3004953)
-println(input.run())
-println()
+val input = 3004953
+println("--- part #1 ---")
+println(solve(input))
 
-println("--- input across ---")
-val input2 = new Exchange(3004953, mode="across")
-println(input2.run())
+println("--- part #2 ---")
+println(solve2(input))
