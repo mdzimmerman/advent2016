1c1f08f0b9dd6330ddd0d3ff749b6be550e1ef9c
Matthew Zimmerman <matt.zimmerman@ccri.com>
2017-01-12 12:04:08 -0500

    Solved both halves of Day 24.

--- 24/day24.scala
+++ 24/day24.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable
 import scala.io.Source
 
 /**
@@ -6,11 +7,14 @@ import scala.io.Source
 
 case class Coord(x: Int, y: Int)
 
+case class Pair(i: Int, j: Int)
+
 class Maze(lines: List[String], points: String) {
   val nrows = lines.length
   val ncols = lines.head.length
   val arr = lines.flatMap(_.toSeq).toSeq
-  var pts = points.toSeq
+  val pts = points.toSeq
+  val ptsDists = calcPairwiseDists(pts)
 
   def get(c: Coord): Option[Char] = get(c.x, c.y)
 
@@ -51,31 +55,82 @@ class Maze(lines: List[String], points: String) {
   }
 
   def bfs(start: Coord, end: Coord): Option[Int] = {
-    val queue = new scala.collection.mutable.Queue[(Coord, Int)]()
+    val visited = new mutable.HashSet[Coord]()
+    val queue = new mutable.Queue[(Coord, Int)]()
+    visited.add(start)
     queue.enqueue((start, 0))
     while (queue.nonEmpty) {
       val (c, length) = queue.dequeue()
+      //println(s"## bfs: $c length=$length")
       for (cNext <- getNextValid(c)) {
         if (cNext == end) {
           return Some(length+1)
-        } else {
+        } else if (!visited.contains(cNext)) {
+          visited.add(cNext)
           queue.enqueue((cNext, length + 1))
         }
       }
     }
     None
   }
+
+  def calcPairwiseDists(points: Seq[Char]) = {
+    val dists = mutable.HashMap[Pair, Int]()
+    val coords = points.map(find)
+    for (i <- 0 until points.length ; j <- i until points.length) {
+      val a = coords(i)
+      val b = coords(j)
+      if ( a == b ) {
+        dists(Pair(i, j)) = 0
+      } else {
+        //println(s"## calc dist between $i-$j")
+        val dist = bfs(a, b).getOrElse(0)
+        dists(Pair(i, j)) = dist
+        dists(Pair(j, i)) = dist
+      }
+    }
+    dists.toMap
+  }
+
+  def printDists() = {
+    print("%5s".format(" "))
+    for (j <- 0 until pts.length)
+      print(" %4d".format(j))
+    println()
+    for (i <- 0 until pts.length) {
+      print("%5d".format(i))
+      for (j <- 0 until pts.length) {
+        print(" %4d".format(ptsDists(Pair(i, j))))
+      }
+      println()
+    }
+  }
+
+  def findMinPath(): Int = {
+    var dmin = 1000000
+    val p0 = (0 until pts.length)
+    val phead = Seq(p0.head)
+    for (ptail <- p0.tail.permutations) {
+      val p = phead ++ ptail ++ phead
+      //println(p.mkString("-"))
+      val d = p.sliding(2).map(s => {
+        val p = Pair(s(0), s(1))
+        ptsDists(p)
+      }).sum
+      println(s"$p => $d")
+      if (d < dmin) dmin = d
+    }
+    dmin
+  }
 }
 
 val test = new Maze(Source.fromFile("test.txt").getLines().toList, "01234")
-test.printMaze()
-val pt0 = test.find('0')
-val pt1 = test.find('1')
-val pt2 = test.find('2')
-val pt3 = test.find('3')
-val pt4 = test.find('4')
+//test.printMaze()
+test.printDists()
+println(test.findMinPath())
 
-println("0 - 1 => "+test.bfs(pt0, pt1))
-println("1 - 2 => "+test.bfs(pt1, pt2))
-println("2 - 3 => "+test.bfs(pt2, pt3))
-println("3 - 4 => "+test.bfs(pt3, pt4))
+val input = new Maze(Source.fromFile("input.txt").getLines().toList, "01234567")
+//input.printMaze()
+input.printDists()
+println(input.findMinPath())
+
